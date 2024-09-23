e4354895ae672f5c66f5240af691a1fbe6348e0f
Julio Silva <juliocesar.silvadi@elorrieta-errekamari.com>
2024-09-23 16:55:42 +0200

    Ejercicio 6 terminado

--- MultiprocesoEjercicioPSP/src/ejercicio1/Ejer5.java
+++ MultiprocesoEjercicioPSP/src/ejercicio1/Ejer5.java
@@ -1,13 +1,15 @@
 package ejercicio1;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Map;
 
 public class Ejer5 {
 
 	public static void main(String[] args) {
-		ProcessBuilder processBuilder = new ProcessBuilder("powershell.exe", "/C", "ps notepad");
+		ProcessBuilder processBuilder = new ProcessBuilder("CMD", "/c", "tasklist");
 
 		try {
 
@@ -16,15 +18,27 @@ public class Ejer5 {
 			
 
 			InputStream inputStream = execComando.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-			System.out.println();
+			String line;
+			String powerShell=null;
 
-			int c;
+			
+			while(null!=(line=bufferedReader.readLine())) {
+				System.out.println(line);
+				powerShell += line;
+			}
+			
+			if(powerShell.contains("Notepad.exe")) {
+				ProcessBuilder ProccessBuilderKill = new ProcessBuilder("CMD", "/c", "taskkill /IM notepad.exe");
+				ProccessBuilderKill.start();
+				System.out.println("Se elimino el notepad");
 
-			while ((c = inputStream.read()) != -1)
-
-				System.out.print((char) c);
-
+			}else {
+				System.out.println("No se elimino el notepad");
+			}
+			
 			inputStream.close();
 			
 
